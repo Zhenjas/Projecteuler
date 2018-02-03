803c0226b4cd539aa1ac8aed01ad022a03a5f18a
Eugene <leon@inpost.lv>
2018-02-03 12:58:21 +0000

    Code refactoring

--- problems.php
+++ problems.php
@@ -1,38 +1,47 @@
-//Problem 1
-//---------------
 <?php
 
-$res = 0;
-for($i=1;$i<1000;$i++){
-	if(($i % 3 == 0) || ($i % 5 == 0)){
-		$res += $i;
-	}
+Class projectEuler
+{
+    const UPPER_LIMIT_ONE = 1000;
+    const UPPER_LIMIT_TWO = 3000000;
+
+    public function problemOne()
+    {
+        $res = 0;
+
+        for ($i = 1; $i < self::UPPER_LIMIT_ONE; $i++) {
+            if (($i % 3 == 0) || ($i % 5 == 0)) {
+                $res += $i;
+            }
+        }
+
+        return $res;
+    }
+
+    public function problemTwo()
+    {
+        $x = 0;
+        $y = 1;
+        $countEvenNums = 0;
+        while (true) {
+            $z = $x + $y;
+
+            if ($z % 2 === 0) {
+                $countEvenNums += $z;
+            }
+
+            if ($z > self::UPPER_LIMIT_TWO) {
+                return $countEvenNums;
+                break;
+            }
+
+            $x = $y;
+            $y = $z;
+        }
+    }
 }
 
-echo $res;
-?>
+$pe = new projectEuler();
 
-//Problem 2
-//---------------
-<?php
-
-$x=0;    
-$y=1; 
-$countEvenNums = 0;
-while(true)    
-{    	
-    $z = $x + $y;
-	
-    if ($z % 2 === 0) {
-        $countEvenNums += $z;
-    }
-	
-    if($z > 3000000){
-		echo $countEvenNums;
-		break;
-    }
-	
-    $x=$y;       
-    $y=$z;
-} 
-?>
+echo "Answer for problem 1 = " . $pe->problemOne() . "\n";
+echo "Answer for problem 2 = " . $pe->problemTwo();
